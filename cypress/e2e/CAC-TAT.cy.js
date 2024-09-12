7e52873325add323c5f5d63af9158b3b2d2127c2
GUSTAVO FERRAZ SANTOS <110568302+gustavoferrazdesousa@users.noreply.github.com>
2024-09-11 22:28:12 -0300

    utilizando .invoke para preencher texto

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -187,4 +187,10 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .invoke('hide')
       .should('not.be.visible')
   })
+
+  it('preenche o campo da área de texto usando o comando invoke', () => {
+    cy.get('#open-text-area')
+      .invoke('val', 'um texto qualquer')
+      .should('have.value', 'um texto qualquer')
+  })
 })
