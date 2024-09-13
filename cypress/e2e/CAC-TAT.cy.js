97181ab490924975afb51dbf9fd15cd3583f48d6
GUSTAVO FERRAZ SANTOS <110568302+gustavoferrazdesousa@users.noreply.github.com>
2024-09-12 22:07:23 -0300

    desafio de encontrar o gato

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -205,7 +205,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     })
   })
 
-  it.only('encontre o gato e mude o title para CAT TAT', () => {
+  it('encontre o gato e mude o title para CAT TAT', () => {
     cy.get('#cat').invoke('show').should('be.visible')
     cy.get('#title').invoke('text', 'CAT TAT')
     cy.get('#subtitle').invoke('text', 'Eu ❤️ gatos')
